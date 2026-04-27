9a9b878d98f007da01a8c756b557ee9dfa019fd9
SAADA-CLINIC <saada.mental.clinic@gmail.com>
2026-04-28 00:50:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const { PeerServer } = require('peer');
 
 const peerServer = PeerServer({
   port: process.env.PORT || 9000,
-  path: '/',
+  path: '/peerjs', // تغيير المسار لمنع التعارض
   allow_discovery: true,
+  proxied: true // هذا السطر حاسم لعمل السيرفر على Render
 });
 
 console.log("SA'ADA Peer Server is running...");
