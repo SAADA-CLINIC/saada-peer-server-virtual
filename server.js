55c07208292f595dcdc7835206813ea6c448e86f
SAADA-CLINIC <saada.mental.clinic@gmail.com>
2026-04-27 23:29:06 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer({
+  port: process.env.PORT || 9000,
+  path: '/',
+  allow_discovery: true,
+});
+
+console.log("SA'ADA Peer Server is running...");
