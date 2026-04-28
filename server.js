ba3f8d09ba86fd9862eb4dd962acc463978ed102
SAADA-CLINIC <saada.mental.clinic@gmail.com>
2026-04-28 20:51:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const peerServer = PeerServer({
   port: process.env.PORT || 9000,
   path: '/peerjs', 
   allow_discovery: true,
-  proxied: true 
+  proxied: true,
+  pingInterval: 5000 // السطر ده هو "النبضة" اللي بتتبعت كل 5 ثواني عشان تمنع قطع الاتصال
 });
 
-// التعديل: الـ 3 سطور دول هيردوا على موقع الـ cron-job عشان ما يديش 404
+// السطور دي الخاصة بالغفير (الكرون) عشان يفضل السيرفر صاحي
 peerServer.get('/', (req, res) => {
   res.status(200).send("SA'ADA Peer Server is Awake!");
 });
